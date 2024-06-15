eabe6156a7a1dd146bedf43dc73a0e11eea450bf
agungd3v <agungd3v@gmail.com>
2024-06-15 17:09:17 +0700

    update

--- resources/views/layouts/static-layout.blade.php
+++ resources/views/layouts/static-layout.blade.php
@@ -19,7 +19,7 @@
   <link rel="stylesheet" href="{{ asset("assets/vendor/css/core.css") }}" class="template-customizer-core-css" />
   <link rel="stylesheet" href="{{ asset("assets/vendor/css/theme-default.css") }}" class="template-customizer-theme-css" />
   <link rel="stylesheet" href="{{ asset("font.css") }}" />
-  <link rel="stylesheet" href="{{ asset("assets/css/main.css") }}" />
+  <link rel="stylesheet" href="{{ asset("assets/css/main.css?v=1.0.1") }}" />
   <link rel="stylesheet" href="{{ asset("assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css") }}" />
   @stack("style")
 </head>
